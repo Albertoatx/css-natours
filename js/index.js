0b7b24a2790bd9836cc1b972d97a54e1988a50eb
amm <f1smydrug@gmail.com>
2019-04-05 22:24:45 +0200

    Building the Navigation, part2. Add JS to close the menu after clicking on a navigation link

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+
+// Make the navigation functional: After clinking a navigation link the menu should close
+var navlinks = document.querySelectorAll('.navigation__link');
+
+Array.from(navlinks).forEach(function (link) {
+  link.addEventListener('click', function () {
+    document.getElementById("navi-toggle").checked = false; // uncheck the checkbox
+  });
+});
